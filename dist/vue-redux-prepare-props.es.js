a0b9e87583f3b4e5139b3bb71364886b09282b50
Anatoly Kolesov <kolesoffac@gmail.com>
2018-05-25 11:21:28 +0300

    Build 0.0.2

---
+++ dist/vue-redux-prepare-props.es.js
@@ -0,0 +1,36 @@
+var preparePropsFromRedux = function preparePropsFromRedux(map) {
+	var subConditions = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+	var prefix = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : "inner_";
+
+	var calcs = {};
+	var watchers = {};
+
+	var _loop = function _loop(prop) {
+		var innerProp = prefix + prop;
+
+		calcs[innerProp] = map[prop];
+		watchers[innerProp] = function (newVal, oldVal) {
+			if (newVal !== undefined && oldVal !== undefined && newVal != oldVal) {
+				if (!subConditions[prop] || subConditions[prop] && typeof subConditions[prop] === "function" && subConditions[prop](newVal, oldVal)) {
+					this[prop] = newVal;
+				}			}		};
+	};
+
+	for (var prop in map) {
+		_loop(prop);
+	}
+	var datas = function datas(context) {
+		var state = context.$store.state;
+
+		var newDatas = {};
+
+		for (var prop in map) {
+			newDatas[prop] = map[prop].call(context, state);
+		}
+		return newDatas;
+	};
+
+	return { calcs: calcs, datas: datas, watchers: watchers };
+};
+
+export default preparePropsFromRedux;
