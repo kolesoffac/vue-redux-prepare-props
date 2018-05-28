957a4538c503b787a9e4e50c0cb89e6074d042a0
Anatoly Kolesov <kolesoffac@gmail.com>
2018-05-28 09:22:05 +0300

    feat(refactor): use 'inner' only for object props

--- src/index.js
+++ src/index.js
@@ -1,29 +1,38 @@
 const prepareProps = function(map, subConditions={}, prefix="inner_") {
 	let calcs = {};
 	let watchers = {};
-
-	for (const prop in map) {
-		const innerProp = prefix+prop;
-
-		calcs[innerProp] = map[prop];
-		watchers[innerProp] = function(newVal, oldVal) {
-			if (newVal !== undefined && oldVal !== undefined && newVal != oldVal) {
-				if (!subConditions[prop] || subConditions[prop] && typeof subConditions[prop] === "function" && subConditions[prop](newVal, oldVal)) {
-					this[prop] = newVal;
-				};
-			};
-		};
-	};
+	let mapObjectProps = {};
 
 	const datas = function(context) {
 		const {$store:{state}} = context;
 		let newDatas = {};
 
 		for (const prop in map) {
-			newDatas[prop] = map[prop].call(context, state);
+			const initialValue = map[prop].call(context, state);
+
+			if (typeof initialValue === "object") {
+				mapObjectProps[prop] = true;
+				newDatas[prop] = initialValue;
+			};
 		};
 
 		return newDatas;
+	};
+
+	for (const prop in map) {
+		const innerProp = prefix+prop;
+
+		calcs[innerProp] = map[prop];
+
+		if (mapObjectProps[prop]) {
+			watchers[innerProp] = function(newVal, oldVal) {
+				if (newVal !== undefined && oldVal !== undefined && newVal != oldVal) {
+					if (!subConditions[prop] || subConditions[prop] && typeof subConditions[prop] === "function" && subConditions[prop](newVal, oldVal)) {
+						this[prop] = newVal;
+					};
+				};
+			};
+		};
 	};
 
 	return {calcs, datas, watchers};
